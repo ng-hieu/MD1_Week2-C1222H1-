c39a9d28c3059ba3704902b6d6d112ed7d236de3
Your Name <you@example.com>
2023-01-05 16:10:27 +0700

    BaitapvaThuchanh

---
+++ BP6/BP6(Baigiang).js
@@ -0,0 +1,5 @@
+let intro="Hello World" /*Chuong trinh dau tien*/
+document.getElementById("intro").innerHTML=intro;
+function on_click (){
+    document.getElementById("demo").innerHTML="Ngạc nhiên chưa????";
+}

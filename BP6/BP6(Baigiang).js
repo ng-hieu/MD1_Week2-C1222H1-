3c931ef2acf4f8c2129d5bcacea610e686461c39
Your Name <you@example.com>
2023-01-06 08:03:45 +0700

    Bai thuc hanh BP7

--- BP6/BP6(Baigiang).js
+++ BP6/BP6(Baigiang).js
@@ -1,5 +1,5 @@
 let intro="Hello World" /*Chuong trinh dau tien*/
 document.getElementById("intro").innerHTML=intro;
-function on_click (){
-    document.getElementById("demo").innerHTML="Ngạc nhiên chưa????";
+function on_click1 (){
+    document.getElementById("demo").innerHTML="Ngạcccc nhiênnnn chưa?????";
 }
